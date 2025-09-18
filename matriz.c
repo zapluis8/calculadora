c16c1e639d774e7294e2ba4c44d3ba46ae620a1c
zapluis8 <obatalam05@gmail.com>
2025-09-18 03:32:34 -0600

    Update matriz.c

--- matriz.c
+++ matriz.c
@@ -76,32 +76,3 @@ Matriz* multiplicarMatriz(Matriz* m1,Matriz* m2){
 int esCuadrada(Matriz* m){
     return m->filas==m->columnas;
 }
-
-// ---------------- Tensores ----------------
-
-// Crear tensor nxm de tamaño profundidad
-Tensor* crearTensor(int profundidad,int filas,int columnas){
-    Tensor* t=(Tensor*)malloc(sizeof(Tensor));
-    t->profundidad = profundidad;
-    t->capas=(Matriz**)malloc(profundidad*sizeof(Matriz*));
-    for(int i=0;i<profundidad;i++)
-        t->capas[i] = crearMatriz(filas,columnas);
-    return t;
-}
-
-// Liberar tensor
-void liberarTensor(Tensor* t){
-    for(int i=0;i<t->profundidad;i++)
-        liberarMatriz(t->capas[i]);
-    free(t->capas);
-    free(t);
-}
-
-// Imprimir tensor
-void imprimirTensor(Tensor* t){
-    for(int i=0;i<t->profundidad;i++){
-        printf("Capa %d:\n",i+1);
-        imprimirMatriz(t->capas[i]);
-        printf("\n");
-    }
-}
