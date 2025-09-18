dd61396d31862f2f37d1697ca5c4d7e9dfafe83f
zapluis8 <obatalam05@gmail.com>
2025-09-18 03:31:42 -0600

    Update tensor.h

--- tensor.h
+++ tensor.h
@@ -1,1 +1,17 @@
+#ifndef TENSOR_H
+#define TENSOR_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int profundidad; // número de matrices en el tensor
+    Matriz **capas;
+} Tensor;
+
+// Funciones para tensores
+Tensor* crearTensor(int profundidad, int filas, int columnas);
+void liberarTensor(Tensor* t);
+void imprimirTensor(Tensor* t);
+
+#endif
